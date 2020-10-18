53ee8e5417f41f006d54c0586cd9b71add1c2494
Hichigo <hichigo91@gmail.com>
2020-10-18 09:51:14 +0300

    refactoring with new module

--- __init__.py
+++ __init__.py
@@ -9,81 +9,18 @@ bl_info = {
     "category": "Node",
 }
 
+
 import bpy
 from bpy.types import Operator, Panel
-from bpy.props import (
-    FloatProperty,
-    EnumProperty,
-    BoolProperty,
-    IntProperty,
-    StringProperty,
-    FloatVectorProperty,
-    CollectionProperty,
-)
-
-from bpy_extras.io_utils import ImportHelper
-
-def check_space(context):
-    space = context.space_data
-    valid_trees = ["ShaderNodeTree", "CompositorNodeTree", "TextureNodeTree"]
-
-    return (space.type == 'NODE_EDITOR' and space.node_tree is not None and space.tree_type in valid_trees)
-
-class NWBase:
-    @classmethod
-    def poll(cls, context):
-        return check_space(context)
-
-class NWAddOctaneSetup(Operator, NWBase, ImportHelper):
-    bl_idname = "node.nw_add_textures_for_octane"
-    bl_label = "Octane Texture Setup"
-    bl_description = "Add Texture Node Setup for Octane"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    directory: StringProperty(
-        name='Directory',
-        subtype='DIR_PATH',
-        default='',
-        description='Folder to search in for image files'
-    )
-    files: CollectionProperty(
-        type=bpy.types.OperatorFileListElement,
-        options={'HIDDEN', 'SKIP_SAVE'}
-    )
-
-    relative_path: BoolProperty(
-        name='Relative Path',
-        description='Select the file relative to the blend file',
-        default=True
-    )
-
-    order = [
-        "filepath",
-        "files",
-    ]
-
-    def draw(self, context):
-        layout = self.layout
-        layout.alignment = 'LEFT'
-
-        layout.prop(self, 'relative_path')
-
-    @classmethod
-    def poll(cls, context):
-        return check_space(context)
-
-    def execute(self, context):
-        pass
-        return {'FINISHED'}
-
+from .AddOctaneTextures.add_octane_textures_op import *
 
 addon_keymaps = []
 kmi_defs = (
-    (NWAddOctaneSetup.bl_idname, 'T', 'PRESS', True, True, False, None, "Add Principled texture setup"),
+    (NWAddOctaneTextures.bl_idname, 'T', 'PRESS', True, True, False, None, "Add Principled texture setup"),
 )
 
 classes = (
-    NWAddOctaneSetup,
+    NWAddOctaneTextures,
 )
 
 def register():
