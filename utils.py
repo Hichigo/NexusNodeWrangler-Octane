bdfdcaff9b6553d1438a22d3a3d28e8fb3270e92
DESKTOP-Q28Q7F1\Ivan <hichigo91@gmail.com>
2020-10-21 13:56:17 +0300

    added name connect input and refactoring quotes

--- utils.py
+++ utils.py
@@ -3,67 +3,78 @@ import bpy
 def get_separators():
     addon_prefs = get_addon_prefs()
     separators = addon_prefs.separators
-    return separators.split(",")
+    return separators.split(',')
 
 def get_node_inputs_with_tags():
     addon_prefs = get_addon_prefs()
     tags = addon_prefs.ocatane_textures_tags
 
     node_inputs_with_tags = {
-        "Transmission": {
-            "tags": tags.transmission.split(' '),
-            "img_path": '',
-            "node": ''
+        'Transmission': {
+            'name_to_connect_input': 'Transmission',
+            'tags': tags.transmission.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Albedo color": {
-            "tags": tags.albedo_color.split(' '),
-            "img_path": '',
-            "node": ''
+        'Albedo color': {
+            'name_to_connect_input': 'Albedo color',
+            'tags': tags.albedo_color.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Ambient occlusion": {
-            "tags": tags.ambient_occlusion.split(' '),
-            "img_path": '',
-            "node": ''
+        'Ambient occlusion': {
+            'name_to_connect_input': 'Albedo color',
+            'tags': tags.ambient_occlusion.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Metallic": {
-            "tags": tags.metallic.split(' '),
-            "img_path": '',
-            "node": ''
+        'Metallic': {
+            'name_to_connect_input': 'Metallic',
+            'tags': tags.metallic.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Specular": {
-            "tags": tags.specular.split(' '),
-            "img_path": '',
-            "node": ''
+        'Specular': {
+            'name_to_connect_input': 'Specular',
+            'tags': tags.specular.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Roughness": {
-            "tags": tags.roughness.split(' '),
-            "img_path": '',
-            "node": ''
+        'Roughness': {
+            'name_to_connect_input': 'Roughness',
+            'tags': tags.roughness.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Gloss": {
-            "tags": tags.gloss.split(' '),
-            "img_path": '',
-            "node": ''
+        'Gloss': {
+            'name_to_connect_input': 'Roughness',
+            'tags': tags.gloss.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Opacity": {
-            "tags": tags.opacity.split(' '),
-            "img_path": '',
-            "node": ''
+        'Opacity': {
+            'name_to_connect_input': 'Opacity',
+            'tags': tags.opacity.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Bump": {
-            "tags": tags.bump.split(' '),
-            "img_path": '',
-            "node": ''
+        'Bump': {
+            'name_to_connect_input': 'Bump',
+            'tags': tags.bump.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Normal": {
-            "tags": tags.normal.split(' '),
-            "img_path": '',
-            "node": ''
+        'Normal': {
+            'name_to_connect_input': 'Normal',
+            'tags': tags.normal.split(' '),
+            'img_path': '',
+            'node': ''
         },
-        "Displacement": {
-            "tags": tags.displacement.split(' '),
-            "img_path": '',
-            "node": ''
+        'Displacement': {
+            'name_to_connect_input': 'Displacement',
+            'tags': tags.displacement.split(' '),
+            'img_path': '',
+            'node': ''
         }
     }
 
@@ -77,4 +88,4 @@ def get_addon_prefs():
     return addon_prefs
 
 def get_addon_name():
-    return __name__.split(".")[0]
+    return __name__.split('.')[0]
