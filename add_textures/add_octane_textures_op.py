80e1a26c2ae3ad59f47511dc9a9d39c9607734ba
DESKTOP-Q28Q7F1\Ivan <hichigo91@gmail.com>
2020-10-21 16:15:50 +0300

    create texture from name

--- add_textures/add_octane_textures_op.py
+++ add_textures/add_octane_textures_op.py
@@ -204,7 +204,7 @@ class NWAddOctaneTextures(Operator, NWBase, ImportHelper):
         """ create texture node and load image """
         if input_obj['img_path']:
             img = bpy.data.images.load(input_obj['img_path'])
-            texture_node = nodes.new(type='ShaderNodeOctFloatImageTex')
+            texture_node = nodes.new(type=input_obj['create_node'])
             texture_node.image = img
             texture_node.label = input_name
             input_obj['node'] = texture_node
