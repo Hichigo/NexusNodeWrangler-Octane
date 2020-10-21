44e1c1fad893fa0733145251c3c31285ff50445e
DESKTOP-Q28Q7F1\Ivan <hichigo91@gmail.com>
2020-10-21 12:54:09 +0300

    added node field

--- utils.py
+++ utils.py
@@ -12,47 +12,58 @@ def get_node_inputs_with_tags():
     node_inputs_with_tags = {
         "Transmission": {
             "tags": tags.transmission.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Albedo color": {
             "tags": tags.albedo_color.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Ambient occlusion": {
             "tags": tags.ambient_occlusion.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Metallic": {
             "tags": tags.metallic.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Specular": {
             "tags": tags.specular.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Roughness": {
             "tags": tags.roughness.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Gloss": {
             "tags": tags.gloss.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Opacity": {
             "tags": tags.opacity.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Bump": {
             "tags": tags.bump.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Normal": {
             "tags": tags.normal.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         },
         "Displacement": {
             "tags": tags.displacement.split(' '),
-            "img_path": ''
+            "img_path": '',
+            "node": ''
         }
     }
 
